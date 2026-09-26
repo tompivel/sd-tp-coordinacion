e7a3d0383a455094f8f849a7e05dfd319a88d360
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-26 12:03:35 -0300

    feat: MessageHandler Client ID tracking, serializing per type

--- golang/src/gateway/messagehandler/messagehandler.go
+++ golang/src/gateway/messagehandler/messagehandler.go
@@ -1,26 +1,37 @@
 package messagehandler
 
 import (
+	"fmt"
+	"sync/atomic"
+
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/fruititem"
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/messageprotocol/inner"
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
 )
 
+var clientCounter uint64
+
 type MessageHandler struct {
+	clientID string
 }
 
 func NewMessageHandler() MessageHandler {
-	return MessageHandler{}
+	id := atomic.AddUint64(&clientCounter, 1)
+	return MessageHandler{
+		clientID: fmt.Sprintf("client-%d", id),
+	}
+}
+
+func (messageHandler *MessageHandler) ClientID() string {
+	return messageHandler.clientID
 }
 
 func (messageHandler *MessageHandler) SerializeDataMessage(fruitRecord fruititem.FruitItem) (*middleware.Message, error) {
-	data := []fruititem.FruitItem{fruitRecord}
-	return inner.SerializeMessage(data)
+	return inner.SerializeDataMessage(messageHandler.clientID, []fruititem.FruitItem{fruitRecord})
 }
 
 func (messageHandler *MessageHandler) SerializeEOFMessage() (*middleware.Message, error) {
-	data := []fruititem.FruitItem{}
-	return inner.SerializeMessage(data)
+	return inner.SerializeEOFMessage(messageHandler.clientID, 0)
 }
 
 func (messageHandler *MessageHandler) DeserializeResultMessage(message *middleware.Message) ([]fruititem.FruitItem, error) {
